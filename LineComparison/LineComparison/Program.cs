617e37bae4503855d0e8e6464881ec15feaf0f7c
Arshad <mohammedarshad183@gmail.com>
2022-05-17 15:50:45 +0530

    welcome to line comparison problem

---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to line comparison problem");
+            Console.ReadLine();
+        }
+    }
+}
