dbc22ef0005c0cb41eaa44657c893cdfee05c9d8
Arshad <mohammedarshad183@gmail.com>
2022-05-17 22:14:33 +0530

    Refactor: checking two lines are equal or not

--- LineComparison/LineComparison/LineComparison.cs
+++ LineComparison/LineComparison/LineComparison.cs
@@ -8,11 +8,22 @@ namespace LineComparison
 {
     class LineComparison
     {
-        public void Line1(int x1, int x2, int y1, int y2)
+        
+        public int Line1(int x1, int x2, int y1, int y2)
         {
             int line1 = (int)Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
             Console.WriteLine($"The length of the line is {line1}");
+            return line1;
         }
+
+        public void check(int line1,int line2)
+        {
+            if(line1.CompareTo(line2) == 0)
+            {
+                Console.WriteLine($"The two line {line1}  and  {line2} are Equal");
+            }
+        }
+       
     }
 
 }
--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -11,7 +11,8 @@ namespace LineComparison
         static void Main(string[] args)
         {
             LineComparison lc = new LineComparison();
-            Console.WriteLine("Enter x and y coordinates to find length of line");
+            //Line one
+            Console.WriteLine("Enter x and y coordinates to find length of line1");
             Console.Write("Enter x1 :  ");
             int x1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter x2 :  ");
@@ -20,7 +21,24 @@ namespace LineComparison
             int y1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Enter y2 :  ");
             int y2 = Convert.ToInt32(Console.ReadLine());
-            lc.Line1(x1, x2, y1, y2);
+            int Line1 = lc.Line1(x1, x2, y1, y2);
+
+            //Line two
+            Console.WriteLine("Enter x and y coordinates to find length of line2");
+            Console.Write("Enter a1 :  ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a2 :  ");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b1 :  ");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b2 :  ");
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int Line2 = lc.Line1(a1, a2, b1, b2);
+
+
+            //condition check
+            lc.check(Line1, Line2);
+            Console.ReadLine();
             Console.ReadLine();
 
 
