d26bcb0c19a55dbadf2f656676e9db6e72c63b23
Arshad <mohammedarshad183@gmail.com>
2022-05-17 15:57:58 +0530

    calculating the length of the line

---
+++ LineComparison/LineComparison/LineComparison.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    class LineComparison
+    {
+        public void Line1(int x1, int x2, int y1, int y2)
+        {
+            int line1 = (int)Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine($"The length of the line is {line1}");
+        }
+    }
+
+}
--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -10,8 +10,20 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to line comparison problem");
+            LineComparison lc = new LineComparison();
+            Console.WriteLine("Enter x and y coordinates to find length of line");
+            Console.Write("Enter x1 :  ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter x2 :  ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter y1 :  ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter y2 :  ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            lc.Line1(x1, x2, y1, y2);
             Console.ReadLine();
+
+
         }
     }
 }
